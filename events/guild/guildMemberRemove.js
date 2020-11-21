d5056896de262d0f84bdef8a91a100c4d873313f
DynamoRed <raphael.biron@hotmail.com>
2020-11-21 04:18:50 +0100

    splife

--- events/guild/guildMemberRemove.js
+++ events/guild/guildMemberRemove.js
@@ -1,5 +1,6 @@
 const Discord = require("discord.js");
 
 module.exports = (bot, m) => {
+    console.log("member quit");
     m.guild.channels.cache.find(c => c.id == bot.config.I_CHANNELS.MEMBER_STATS).setName(`👥 Membres: ${m.guild.memberCount}`);
 }

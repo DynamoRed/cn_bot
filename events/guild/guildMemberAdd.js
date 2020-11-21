170b3599c8a121ceaab4aff0870064f46be67316
DynamoRed <raphael.biron@hotmail.com>
2020-11-21 04:28:45 +0100

    splife

--- events/guild/guildMemberAdd.js
+++ events/guild/guildMemberAdd.js
@@ -7,18 +7,4 @@ module.exports = (bot, m) => {
         .setDescription(`<@${m.user.id}> vient d'arriver sur notre discord ! :fire:`);
 
     m.guild.channels.cache.find(c => c.id == bot.config.I_CHANNELS.WELCOME).send(memberAddEmbed);
-
-    //CAPTCHA EMBEDS
-    let captchaEmbed1 = new Discord.MessageEmbed()
-        .setColor(bot.config.COLORS.BASE)
-        .setImage("https://i.imgur.com/Y4L6xDZ.png");
-        
-    let captchaEmbed2 = new Discord.MessageEmbed()
-        .setColor(bot.config.COLORS.BASE)
-        .setTitle(`🔐 Vérification`)
-        .setDescription(`${bot.botEmojis.GLOBAL.BULLET} **Bienvenue <@${m.user.id}> sur SPLife !**
-        Veuillez réagir sous le message dans <#${bot.config.I_CHANNELS.VERIFICATION}> avec ✅ pour passer notre captcha.`);
-
-    m.user.send(captchaEmbed1);
-    m.user.send(captchaEmbed2);
 }

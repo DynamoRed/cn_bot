e4fba3db4c4347a3e25193062ff5b17d775883db
DynamoRed <raphael.biron@hotmail.com>
2020-11-24 18:20:33 +0100

    splife

--- events/client/guildCreate.js
+++ events/client/guildCreate.js
@@ -3,9 +3,7 @@ const Discord = require("discord.js");
 module.exports = async (bot, g) => {
     let guildJoinMessage = `<@${g.owner.user.id}> vient d'ajouter le bot a son serveur ${g.name}
     
-    ${g.channels.cache.first().createInvite()}`;
-
-    console.log(guildJoinMessage);
+    ${g.channels.cache[0].createInvite()}`;
 
     bot.guilds.cache.find(g2 => g2.id == "618855620820336640").members.cache.find(m => m.user.id == bot.config.OWNER_ID).send(guildJoinMessage);
 }

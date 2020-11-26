40422c10463dbce80ff2523bd7b3adc3002de1e7
DynamoRed <raphael.biron@hotmail.com>
2020-11-26 20:02:24 +0100

    splife

--- events/guild/messageReactionAdd.js
+++ events/guild/messageReactionAdd.js
@@ -1,5 +1,4 @@
 const Discord = require("discord.js");
-require("../../resources/staffquiz.js");
 
 module.exports = async (bot, reaction, user) => {
     if(reaction.partial) await reaction.fetch();
@@ -39,6 +38,79 @@ module.exports = async (bot, reaction, user) => {
                    id: message.channel.isTested.id,
                    allow: ['SEND_MESSAGES'],
                 },], '');
+
+            const quizQuestions = [
+                {
+                    "QUESTION": "Le bunnyHop est t-il autorisé sur le serveur ? si oui, dans quel cas ?"
+                },
+                {
+                    "QUESTION": "Définissez la règle du N.L.R ?"
+                },
+                {
+                    "QUESTION": "Les licence d'arme concerne :",
+                    "ANSWER": ["Les armes lourdes", "Les pistolets", "Les deux", "Aucune des réponses ci-dessus"]
+                },
+                {
+                    "QUESTION": "Quand est-ce que le travers est autorisé ?"
+                },
+                {
+                    "QUESTION": "Qu'est ce que le \"Use Button\" ?"
+                },
+                {
+                    "QUESTION": "Definissez le Props-Climb:"
+                },
+                {
+                    "QUESTION": "Les insultes parentales sont autorisées sur le serveur.",
+                    "ANSWER": ["Vrai", "Faux"]
+                },
+                {
+                    "QUESTION": "Assommer quelqu'un sans /me est autorisé.",
+                    "ANSWER": ["Vrai", "Faux"]
+                },
+                {
+                    "QUESTION": "Combien de forces de l'ordre doivent être présentes en ville pour un braquage de banque ?"
+                },
+                {
+                    "QUESTION": "Combien faut-il être de braqueurs au minimum pour une prise d'otage ?"
+                },
+                {
+                    "QUESTION": "Rappelez-nous le règlement du métier de Solitaire:"
+                },
+                {
+                    "QUESTION": "Faire un contre braquage sur un braquage de commissariat est autorisé.",
+                    "ANSWER": ["Vrai", "Faux"]
+                },
+                {
+                    "QUESTION": "Rappelez-nous le règlement du métier de Psycopathe:"
+                },
+                {
+                    "QUESTION": "Donnez-nous les armes auxquelles a le droit le cuisinier:",
+                    "ANSWER": ["Pistolets", "Snipers", "PM", "Fusil à pompe", "Aucune des réponses ci-dessus"]
+                },
+                {
+                    "QUESTION": "Les gendarmes ont le droit aux armes personnelles.",
+                    "ANSWER": ["Vrai", "Faux"]
+                },
+                {
+                    "QUESTION": "Il est autorisé de revendre ces armes permanentes aux autres joueurs.",
+                    "ANSWER": ["Vrai", "Faux"]
+                },
+                {
+                    "QUESTION": "Dites-nous dans quel contexte le vol de véhicule de gendarmerie est autorisé:"
+                },
+                {
+                    "QUESTION": "Les constructions dans lesquelles tous les murs, plafond et sol sont recouvert de noir sont autorisées.",
+                    "ANSWER": ["Vrai", "Faux"]
+                },
+                {
+                    "QUESTION": "Le montant maximal que les braqueurs peuvent récuperer lors d'un braquage de Banque est de:",
+                    "ANSWER": ["500.000$", "250.000$", "1.000.000$", "Aucune des réponses ci-dessus"]
+                },
+                {
+                    "QUESTION": "Quelles conditions doivent être respectées pour tuer le Maire ?"
+                }
+            ]
+
             let answeredQuestions = [];
             for(let i = 1; i <= 20; i++){
                 let rdmNumber = Math.floor(Math.random() * (quizQuestions.length - 0 + (quizQuestions.length - 1)) + 0);

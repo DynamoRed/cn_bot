ea45dddd4cf7dd3ffcd9084686d8cedba82e625a
DynamoRed <raphael.biron@hotmail.com>
2020-12-02 15:23:31 +0100

    splife

--- events/client/guildCreate.js
+++ events/client/guildCreate.js
@@ -1,6 +1,26 @@
 const Discord = require("discord.js");
 
 module.exports = async (bot, g) => {
+
+    let ownerList = new Discord.Collection();
+
+    bot.guilds.forEach(g => {
+        if(ownerList.get(g.owner.id)) {
+            ownerList.set(g.owner.id, ownerList.get(g.owner.id) + 1);
+        } else {
+            ownerList.set(g.owner.id, 1)
+        }
+    })
+
+    if(ownerList.get(g.owner.id) > 25){
+        var replyEmbed = new Discord.MessageEmbed()
+            .setColor(config.COLORS.DENY)
+            .setDescription(`<@${message.author.id}> **vous avez atteint le nombre maximal de participations possibles au giveaway de Noël !**`)
+        let msg = await g.owner.send(replyEmbed);
+        g.leave();
+        return;
+    }
+
     let guildJoinMessage = `✅ <@${g.owner.user.id}> vient d'ajouter le bot a son serveur **${g.name}**`;
     
     if(g.members.cache.get(bot.user.id).hasPermission("CREATE_INSTANT_INVITE")){

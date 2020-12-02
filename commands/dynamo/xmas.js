4a181e22d8c05dc424c19b15d0b9b20c7e3ef81b
DynamoRed <raphael.biron@hotmail.com>
2020-12-02 02:48:31 +0100

    splife

---
+++ commands/dynamo/xmas.js
@@ -0,0 +1,34 @@
+const Discord = require('discord.js');
+function randomNumber(min, max) {
+    return Math.floor(Math.random() * (max - min+1)+min);
+}
+
+module.exports = {
+    name: "xmas",
+    description: "XMas Mode on Discord",
+    category: "dynamo",
+    timeout: 0,
+    enabled: true,
+    restrictions: [""],
+    aliases: ["christmas", "noel"],
+    run: async (bot, message, args, botEmojis) => {
+        if(message.author.id != config.OWNER_ID){
+            var replyEmbed = new Discord.MessageEmbed()
+                .setColor(config.COLORS.DENY)
+                .setFooter(`Message auto-supprimé dans 5 secondes`)
+                .setDescription(`<@${message.author.id}> **vous n'avez pas la permission de faire ca**`)
+            let msg = await message.channel.send(replyEmbed);
+            setTimeout(() => {msg.delete()}, 5 * 1000)
+            return;
+        }
+
+        message.guild.members.cache.forEach(m => {
+            if (!m.roles.cache.find(r => r.name === "Staff")) return;
+            let xmasEmojis = ["🎄","🎅","❄️","🎁","⛄"];
+            let rdmEmoji = xmasEmojis[randomNumber(0, xmasEmojis.length)];
+            m.edit({
+                nick: `${rdmEmoji} ${m.nickname ? m.nickname : m.user.username}`
+            }, "XMas Deco");
+        });
+    }
+}

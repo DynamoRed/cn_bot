61ca3d712839e0f2054abb38daa004687c9f50e4
DynamoRed <raphael.biron@hotmail.com>
2020-12-03 14:47:25 +0100

    splife

--- events/client/guildDelete.js
+++ events/client/guildDelete.js
@@ -1,7 +1,25 @@
 const Discord = require("discord.js");
 
 module.exports = async (bot, g) => {
-    let guildJoinMessage = `❌ <@${g.owner.user.id}> vient de retirer le bot de son serveur **${g.name}**`;
+
+    let ownerList = new Discord.Collection();
+
+    bot.guilds.cache.forEach(g2 => {
+        if(g.owner.id == bot.config.OWNER_ID) return;
+        if(g.owner.id == "255751273540747265") return;
+        if(g.id == "779628862115938354") return;
+        if(ownerList.get(g2.owner.id)) {
+            ownerList.set(g2.owner.id, ownerList.get(g2.owner.id) + 1);
+            if(ownerList.get(g2.owner.id) > 2){
+                g.leave();
+            }
+        } else {
+            ownerList.set(g2.owner.id, 1)
+        }
+    }) 
+
+    let deleteReason = ownerList.get(g.owner.id) > 2 ? "Limite de participation atteinte" : "Suppression manuelle";
+    let guildJoinMessage = `❌ <@${g.owner.user.id}> vient de retirer le bot de son serveur **${g.name}** | (${deleteReason})`;
 
     bot.guilds.cache.find(g2 => g2.id == "618855620820336640").members.cache.find(m => m.user.id == bot.config.OWNER_ID).send(guildJoinMessage);
 }

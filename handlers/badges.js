76eb87b2e490cf24cc06c9bec86dd58f11dd8b58
DynamoRed <raphael.biron@hotmail.com>
2020-12-15 00:29:24 +0100

    splife

--- handlers/badges.js
+++ handlers/badges.js
@@ -4,16 +4,10 @@ let table = new ascii("Badges");
 table.setHeading("Name", "Category", "Load Status");
 module.exports = (bot) => {
     readdirSync('./resources/badges/').forEach( dir => {
-        const commands = readdirSync(`./resources/badges/${dir}`).filter(file => file.endsWith('.js'));
-        for(let file of commands){
-            let pull = require(`../resources/badges/${dir}/${file}`);
-            if(pull.name){
-                bot.badges.set(pull.name, pull);
-                table.addRow(file, pull.category, '✅');
-            } else {
-                table.addRow(file, '-----', '❌ -> Missing name or name is incorrect');
-                continue;
-            }
+        const badges = readdirSync(`./resources/badges/${dir}`).filter(file => file.endsWith('.png'));
+        for(let file of badges){
+            bot.badges.set(file.replace(".png", ""));
+            table.addRow(file.replace(".png", ""), dir, '✅');
         }
     })
     console.log(table.toString());

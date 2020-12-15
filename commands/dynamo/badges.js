4f14f4d5266bc64a64098ad75a0698c43ecc7f15
DynamoRed <raphael.biron@hotmail.com>
2020-12-15 03:21:26 +0100

    splife

--- commands/dynamo/badges.js
+++ commands/dynamo/badges.js
@@ -127,7 +127,7 @@ module.exports = {
                         }
 
                         let obtainedDate = new Date();
-                        console.log(obtainedDate.toLocaleDateString('fr-FR')); 
+                        console.log(obtainedDate.toLocaleString('en-GB', { timeZone: 'UTC+1' }));
                         let badgeRef = bot.badgesData[mentionned.id].badges.length;
                         bot.badgesData[mentionned.id].badges[badgeRef] = {
                             id: args[2],

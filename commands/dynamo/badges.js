439be167efb3224860d5a0eedbbc9884a4a236fe
DynamoRed <raphael.biron@hotmail.com>
2020-12-16 21:54:27 +0100

    splife

--- commands/dynamo/badges.js
+++ commands/dynamo/badges.js
@@ -68,7 +68,7 @@ module.exports = {
                 if(mentionned){
                     bot.db.query(`SELECT * FROM discord_badges WHERE badge_owner="${mentionned.id}"`, async function(err, results){
                         if (err) throw err;
-                        console.log(results);
+                        console.log(results.length);
                         if(results === undefined || results.length == 0){
                             let badge = bot.badges.get(results[0].badge_name);
                             let obtainedDate =  results[0].badge_get_at;

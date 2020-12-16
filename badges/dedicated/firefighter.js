b16ccb314a743b8f2d6c2ba213973c6cb42d3313
DynamoRed <raphael.biron@hotmail.com>
2020-12-17 00:54:46 +0100

    splife

---
+++ badges/dedicated/firefighter.js
@@ -0,0 +1,8 @@
+const Discord = require('discord.js');
+module.exports = {
+    name: "Pompier",
+    description: "C'est un pompier. Oui; aussi simple que ça",
+    category: "dedicated",
+    enabled: true,
+    id: "firefighter"
+}

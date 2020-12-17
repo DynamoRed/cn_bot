6086a0b4dbb8f996059949bdfa6180e2e77436e7
DynamoRed <raphael.biron@hotmail.com>
2020-12-17 02:16:03 +0100

    splife

--- events/client/ready.js
+++ events/client/ready.js
@@ -233,33 +233,7 @@ module.exports = async bot => {
         bot.db.query(`SELECT * FROM discord_badges WHERE badge_owner='${g.owner.user.id}' AND badge_name='event_xmas_2020_v2'`, async function(err, results){
             if (err) throw err;
             if(results === undefined || results.length == 0){
-                let obtainedDate = new Date();
-                obtainedDate = obtainedDate.toLocaleString('en-GB', { timeZone: 'Europe/Paris' });
-                let hours = parseInt(obtainedDate.split(",")[1].split(":")[0]);
-                if(obtainedDate.split(",")[1].split(":")[2].includes("PM")){
-                    hours = hours + 12;
-                    if(hours == 24){
-                        hours = 0;
-                    }
-                }
-                obtainedDate = [obtainedDate.split(",")[0].split("/")[1],
-                obtainedDate.split(",")[0].split("/")[0],
-                obtainedDate.split(",")[0].split("/")[2],
-                hours,
-                obtainedDate.split(",")[1].split(":")[1]];
-                obtainedDate = obtainedDate.join("-");
-
-                bot.db.query(`INSERT INTO discord_badges (badge_name, badge_get_at, badge_owner) VALUES ('event_xmas_2020_v2', '${obtainedDate}', '${g.owner.user.id}')`, async function(err, results){
-                    if (err){
-                        throw err
-                    } else {
-                        let confirmEmbed = new Discord.MessageEmbed()
-                            .setColor(bot.config.COLORS.ALLOW)
-                            .setFooter("Consultez vos badges avec !badges")
-                            .setDescription(`<@${g.owner.user.id}> **vous venez d'acquerir le badge __Event Noël 2020__**`)
-                        let confirmMessage = await g.owner.user.send(confirmEmbed);
-                    }
-                })
+                console.log(g.owner.user.id)
             }
         })
         if(g.owner.id == bot.config.OWNER_ID) return;

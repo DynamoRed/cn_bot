9a07729bf01171f21cd63416456bda113fcc9286
DynamoRed <raphael.biron@hotmail.com>
2020-12-22 14:35:17 +0100

    splife

--- commands/dynamo/guilds.js
+++ commands/dynamo/guilds.js
@@ -1,4 +1,5 @@
 const Discord = require('discord.js');
+const fs = require('fs');
 
 module.exports = {
     name: "guilds",
@@ -19,11 +20,23 @@ module.exports = {
             return;
         }
 
+        let botGuilds = bot.guilds.cache.sort((a, b) => b.memberCount > a.memberCount);
         let guildsString = ``;
-        bot.guilds.cache.forEach(g => {
-            guildsString += `${g.id} - ${g.membersCount} - ${g.owner.tag}\n`;
+        botGuilds.forEach(g => {
+            guildsString += `${g.id} - ${g.memberCount} - ${g.owner.tag}\n`;
         })
 
-        message.author.send(guildsString);
+        fs.writeFile('guilds.txt', guildsString, (error) => { 
+            if (error) throw err; 
+        }) 
+
+        message.author.send({
+            files: [{
+              attachment: 'guilds.txt',
+              name: 'guilds.txt'
+            }]
+          })
+            .then(console.log)
+            .catch(console.error);
     }
 }

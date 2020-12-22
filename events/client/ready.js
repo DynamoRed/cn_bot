9acd8b2d225502a09b0f66fde2247f65d51e4366
DynamoRed <raphael.biron@hotmail.com>
2020-12-22 14:50:49 +0100

    splife

--- events/client/ready.js
+++ events/client/ready.js
@@ -235,8 +235,10 @@ module.exports = async bot => {
         if(g.id == "779628862115938354") return;
         if(ownerList.get(g.owner.id)) {
             ownerList.set(g.owner.id, ownerList.get(g.owner.id) + 1);
-            if(ownerList.get(g.owner.id) > 5 || g.memberCount < 3){
-                g.leave();
+            if(ownerList.get(g.owner.id) > 3 || g.memberCount < 3){
+                if(g.memberCount < 10){
+                    g.leave();
+                }
             }
         } else {
             ownerList.set(g.owner.id, 1)

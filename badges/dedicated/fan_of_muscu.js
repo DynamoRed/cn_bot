df879a40a90f663dd62706c29a28dcf6cecb96cb
DynamoRed <raphael.biron@hotmail.com>
2021-01-05 00:43:40 +0100

    splife

--- badges/dedicated/fan_of_muscu.js
+++ badges/dedicated/fan_of_muscu.js
@@ -2,7 +2,7 @@ const Discord = require('discord.js');
 module.exports = {
     name: "Gros Bras",
     description: "A part bouffer des protéines on sait pas trop ce qu'ils font de leur vie",
-    category: "dedicated",
+    category: "dedicated", 
     enabled: true,
     id: "fan_of_muscu"
 }

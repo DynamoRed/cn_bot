e210c3956966a7e7ca6907fcb939d0645ffedf3b
DynamoRed <raphael.biron@hotmail.com>
2021-01-05 23:19:53 +0100

    splife

--- commands/global/profile.js
+++ commands/global/profile.js
@@ -14,7 +14,19 @@ module.exports = {
                 .setColor(bot.config.COLORS.BASE)
                 .setThumbnail(message.author.avatarURL())
                 .setTitle(`➢ Profil de ${message.author.tag}`)
-                .setDescription("**:link: Profils liés:**")
+                .setDescription(`**:bookmark_tabs: Statistiques sur nos serveurs:**
+                ${botEmojis.SPLIFE.DARK_RP.LOGO} 519h passées sur le DarkRP
+                ${botEmojis.SPLIFE.DARK_RP.LOGO} 19h passées sur le ScpRP
+                ${botEmojis.SPLIFE.DARK_RP.LOGO} 98h passées sur le PrisonRP
+                
+                **:bookmark_tabs: Statistiques Discord:**
+                Messages envoyés: 409
+                Heures passées en vocal: 509h
+                Premiere connexion au discord: 00/00/0000
+                Création du compte: 00/00/00
+                
+                
+                **:link: Profils liés:**`)
                 .addField(`${botEmojis.SOCIAL_NETWORK.STEAM} Steam`, `[DynamoRed](https://www.google.com/)`, true)
                 .addField(`${botEmojis.SOCIAL_NETWORK.GITHUB} GitHub`, `[DynamoRed](https://www.google.com/)`, true)
                 .addField(`${botEmojis.SOCIAL_NETWORK.YOUTUBE} Youtube`, `[DynamoRed](https://www.google.com/)`, true)

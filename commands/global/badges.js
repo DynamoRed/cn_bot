974741811aee19989d69ce72d6b5a7d5a4322fc8
DynamoRed <raphael.biron@hotmail.com>
2021-01-05 19:03:21 +0100

    splife

--- commands/global/badges.js
+++ commands/global/badges.js
@@ -23,7 +23,6 @@ module.exports = {
 
         let mentionned = message.mentions.users.first();
         if(args.length == 0){
-            console.log(message.author.mfa_enabled);
             bot.db.query(`SELECT * FROM discord_badges WHERE badge_owner='${message.author.id}'`, async function(err, results){
                 if (err) throw err;
                 if(results != undefined && results.length != 0){
---
+++ commands/global/profile.js
@@ -0,0 +1,36 @@
+const Discord = require('discord.js');
+module.exports = {
+    name: "profile",
+    description: "Affiche le profil d'un joueur",
+    category: "global",
+    timeout: 120000,
+    enabled: true,
+    restrictions: [""],
+    aliases: ["p", "profiles"],
+    run: async (bot, message, args, botEmojis) => {
+        let mentionned = message.mentions.users.first();
+        if(args.length == 0){
+            let profileEmbed = new Discord.MessageEmbed()
+                .setColor(bot.config.COLORS.BASE)
+                .setThumbnail(message.author.avatarURL())
+                .setTitle(`➢ Profil de ${message.author.tag}`)
+                .setDescription("**:link: Profils liés:**")
+                .addField('Steam', `[DynamoRed](https://www.google.com/)`, true)
+                .addField('GitHub', `[DynamoRed](https://www.google.com/)`, true)
+                .addField('Youtube', `[DynamoRed](https://www.google.com/)`, true)
+                .setTimestamp(2)
+        } else if(args.length == 1){
+            if(mentionned){
+                
+            } else {
+                var replyEmbed = new Discord.MessageEmbed()
+                    .setColor(bot.config.COLORS.DENY)
+                    .setFooter(`Message auto-supprimé dans 5 secondes`)
+                    .setDescription(`<@${message.author.id}> **le membre spécifié n'a pas pu être trouvé !**`)
+                let msg = await message.channel.send(replyEmbed);
+                setTimeout(() => {msg.delete()}, 5 * 1000)
+                return;
+            }
+        }
+    }
+}

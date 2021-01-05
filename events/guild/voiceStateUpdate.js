86d8070c96182d4de2f43adfb5ccaff32e918107
DynamoRed <raphael.biron@hotmail.com>
2021-01-05 18:10:23 +0100

    splife

--- events/guild/voiceStateUpdate.js
+++ events/guild/voiceStateUpdate.js
@@ -1,11 +1,8 @@
 module.exports = (bot, oldState, newState) => {
     console.log(newState.channelID)
     if(newState.channelID != undefined) {
-        console.log("11")
        if(newState.channelID == bot.config.I_CHANNELS.CREATE_CHANNEL){
-        console.log("22")
            var name = newState.member.nickname ? newState.member.nickname : newState.member.user.username;
-           console.log("33")
             newState.channel.guild.createChannel(`🔊・Canal de ` + name, {
                 type: 'voice',
                 permissionOverwrites: [

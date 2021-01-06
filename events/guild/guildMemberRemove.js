dbf293ca5a9f6da3fb839fe81949c7b512c60b8c
DynamoRed <raphael.biron@hotmail.com>
2021-01-06 12:46:50 +0100

    splife

--- events/guild/guildMemberRemove.js
+++ events/guild/guildMemberRemove.js
@@ -1,6 +1,6 @@
 const Discord = require("discord.js");
 
-module.exports = (bot, m) => {
+module.exports = async (bot, m) => {
     if(m.guild.id != "693198481086480544" && m.guild.id != "618855620820336640"){
         return; 
     }

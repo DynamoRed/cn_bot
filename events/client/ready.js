ab0503f1cdb6d03baa18fdbefdd331f4e20b19c6
DynamoRed <raphael.biron@hotmail.com>
2021-01-07 12:56:46 +0100

    splife

--- events/client/ready.js
+++ events/client/ready.js
@@ -17,7 +17,7 @@ module.exports = async bot => {
         onlineCountMembers += bot.guilds.cache.get(bot.config.OFFICIALS_SERVERS.DARKRP).members.cache.filter(m => m.presence.status == "idle").array().length;
         onlineCountMembers += bot.guilds.cache.get(bot.config.OFFICIALS_SERVERS.DARKRP).members.cache.filter(m => m.presence.status == "dnd").array().length;
         bot.guilds.cache.get(bot.config.OFFICIALS_SERVERS.DARKRP).channels.cache.get(bot.config.I_CHANNELS.ONLINE_STATS).setName(`🟢 En Ligne: ${onlineCountMembers}`, "Actualisation Stats")
-    }, 10000);
+    }, 5000);
 
     bot.guilds.cache.get(bot.config.OFFICIALS_SERVERS.DARKRP).channels.cache.get(bot.config.I_CHANNELS.MEMBERS_STATS).setName(`👥 Membres: ${bot.guilds.cache.get(bot.config.OFFICIALS_SERVERS.DARKRP).memberCount}`, "Actualisation Stats")
     let baseGuild = bot.guilds.cache.find(g => g.id == "618855620820336640");

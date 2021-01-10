2664b6b506831a28776dad2fe94f7083a113c85f
DynamoRed <raphael.biron@hotmail.com>
2021-01-11 00:40:24 +0100

    splife

--- events/client/ready.js
+++ events/client/ready.js
@@ -99,9 +99,9 @@ module.exports = async bot => {
 
     bot.getServerChannel = function(id, referTo){
         let result;
-        console.log(id + " V " + referTo)
         bot.db.query(`SELECT * FROM servers_channels_config WHERE server_id='${id}' and refer_to='${referTo}'`, async function(err, results){
             if (err) throw err;
+            console.log(results)
             if(results != undefined && results.length == 1){
                 result = results[0].channel_id;
             } else {
